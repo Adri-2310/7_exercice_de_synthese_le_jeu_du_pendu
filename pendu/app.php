1d02e9b0933e334d5ab526f6c11dcadfd23b56fd
Adrien Mertens <Mertensadrien23@hotmail.be>
2025-09-18 12:39:57 +0200

    Initial commit

---
+++ pendu/app.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Ce fichier contient la fonction principale du jeu du pendu.
+
+    Vous êtes encouragé à organiser les fonctions secondaires dans d'autres fichiers 
+    et à utiliser les outils d'importation appropriés pour structurer votre code de manière claire et efficace.
+*/
+?>
